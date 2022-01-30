cef805436bac8fe921b52be5f1f10dc358bcb5fa
Cherkashin Alexander <cherkalexander@gmail.com>
2022-01-30 12:00:38 +0300

    add ability to specify whitelist of origins

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
+
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: originWhitelist, // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2'],
 }).listen(port, host, function() {
