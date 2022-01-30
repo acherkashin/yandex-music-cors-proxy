1ef14144795ce37484106ce122443fafcac7dbed
Cherkashin Alexander <cherkalexander@gmail.com>
2022-01-30 13:34:40 +0300

    don't pass X-Forwarded-For header

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
@@ -8,14 +8,18 @@ function parseEnvList(env) {
   if (!env) {
     return [];
   }
-  return env.split(',');
+  return env.split(",");
 }
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
     originWhitelist: originWhitelist, // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, othewise yandex.music will return "400 - Bad Request - Contradictory sheme headers"
+      xfwd: false,
+    },
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
